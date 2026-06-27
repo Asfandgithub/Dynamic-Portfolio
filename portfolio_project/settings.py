21ee96d4d7f426a96b7b0b288d19f28481de4386
Asfandgithub <asfandch80@gmail.com>
2026-06-27 02:25:24 -0700

    Fix Django storage configuration

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -143,8 +143,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
-        "BACKEND":
-        "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
